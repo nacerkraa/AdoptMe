481d2378ace4a5656217f958eddfbf624be97055
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2023-02-25 04:09:48 +0100

    Add useBreedList

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,16 +1,15 @@
 import { useState, useEffect } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreeList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
-
   const [breed, setBreed] = useState("");
-  const breeds = ["hamid"];
-
   const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
