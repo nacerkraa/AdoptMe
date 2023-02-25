12b41065468a2904dc4a03c37ddbeb74dc90e319
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2023-02-25 19:21:35 +0100

    Add fetch method

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,31 @@ import { createRoot } from "react-dom/client";
 import { Link,BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient(
+  defaultOptions: {
+    queries:{
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+)
 
 const App = () => {
   return (
     
     <BrowserRouter>
-    <header>
-      <Link to="/">Adopt Me!</Link>
-    </header>
-      <h1>Adopt Me!</h1>
-        <Routes>
-          <Route path="/details/:id"  element={<Details/>} />
-          <Route path="/"  element={<SearchParams/>} />
-        </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+          <h1>Adopt Me!</h1>
+            <Routes>
+              <Route path="/details/:id"  element={<Details/>} />
+              <Route path="/"  element={<SearchParams/>} />
+            </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
