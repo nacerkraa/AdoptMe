55f44b92711df9ac2cf6ecab1defe58ccc9ebcb9
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2023-02-25 17:55:12 +0100

    Add useParams Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link,BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
@@ -7,6 +7,9 @@ const App = () => {
   return (
     
     <BrowserRouter>
+    <header>
+      <Link to="/">Adopt Me!</Link>
+    </header>
       <h1>Adopt Me!</h1>
         <Routes>
           <Route path="/details/:id"  element={<Details/>} />
