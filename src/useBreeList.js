eec29ebf1a35983b2533844d730289d1d98fbde7
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2023-02-26 21:13:02 +0100

    add fetch breed list

--- src/useBreeList.js
+++ src/useBreeList.js
@@ -1,34 +1,8 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import fetchbreedList from "./fetchbreedList";
 
-const localCache = {};
 
 export default function useBreedList(animal){
-    const [breedList, setBreedList] = useState([]);
-    const [status, setStatus] = useState("unloaded");
-
-    useEffect(() => {
-        if (!animal) {
-            setBreedList([]);
-        } else if (localCache[animal]){
-            setBreedList(localCache[animal]);
-        }else{
-            requestBreedList();
-        }
-
-        async function requestBreedList() {
-            setBreedList([]);
-            setStatus("loading");
-            const res = await fetch(
-                `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-            );
-            const json = await res.json();
-            localCache[animal] = json.breeds || [];
-            setBreedList(localCache[animal]);
-            setStatus("loaded");
-        }
-
-        
-
-    }, [animal]);
-    return [breedList, status];
+    const results = useQuery(["breeds", animal], fetchbreedList);
+    return [results?.data?.breeds ?? [], results.status];
 }
