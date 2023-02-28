5f472d7f9bb3767fc306a25c8addad9713038c83
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2023-02-28 18:33:30 +0100

    Add uncontrolled Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import { Link,BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,19 +15,19 @@ const queryClient = new QueryClient({
 
 const App = () => {
   return (
-    
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-          <h1>Adopt Me!</h1>
-            <Routes>
-              <Route path="/details/:id"  element={<Details/>} />
-              <Route path="/"  element={<SearchParams/>} />
-            </Routes>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+
 const Pet = (props) => {
   const { name, animal, breed, images, location, id } = props;
 
