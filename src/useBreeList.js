24daae84625531d79a8fa6553d5b966a48e3f476
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2023-04-24 09:29:57 +0100

    use npx install

--- src/useBreeList.js
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import fetchbreedList from "./fetchbreedList";
-
-
-export default function useBreedList(animal){
-    const results = useQuery(["breeds", animal], fetchbreedList);
-    return [results?.data?.breeds ?? [], results.status];
-}
